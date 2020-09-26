92210bb1782ff65055a941d1bda602c899cf2d88
SDCard <sam.drage@yahoo.com>
2020-09-26 23:56:12 +0100

    Fixed alignment issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,40 +2,41 @@ import React from 'react';
 import Showdown from 'showdown';
 
 function parseMD()    {
-    let text = document.querySelector("#post-markdown").value;
+    let title = document.querySelector("#title-input").value;
+    let author = document.querySelector("#author-input").value;
+    let date = document.querySelector("#date-input").value;
+    let body = document.querySelector("#post-markdown").value;
+    let text = `# ${title} \n### ${author} | ${date}\n${body}`
     let converter = new Showdown.Converter();
-    let markup = converter.makeHtml(text);
-    document.querySelector("#render").innerHTML = markup;
+    document.querySelector("#render").innerHTML = converter.makeHtml(text);
 }
 
 function HomePage() {
     return (
-        <>
-            <body>
-                <section id="editor">
-                    <div id="title-div">
-                        <label htmlFor="title-input">Title</label>
-                        <input type="text" name="title" id="title-input"/>
-                    </div>
+        <div id="main-frame">
+            <section id="editor">
+                <div id="title-div">
+                    <label htmlFor="title-input">Title</label>
+                    <input type="text" name="title" id="title-input" onChange={parseMD}/>
+                </div>
 
-                    <div id="author-div">
-                        <label htmlFor="author-input">Author</label>
-                        <input id="author-input" type="text" name="author"/>
-                    </div>
+                <div id="author-div">
+                    <label htmlFor="author-input">Author</label>
+                    <input id="author-input" type="text" name="author" onChange={parseMD}/>
+                </div>
 
-                    <div id="date-div">
-                        <label htmlFor="date-input">Date</label>
-                        <input id="date-input" type="text" name="date"/>
-                    </div>
+                <div id="date-div">
+                    <label htmlFor="date-input">Date</label>
+                    <input id="date-input" type="text" name="date" onChange={parseMD}/>
+                </div>
 
-                    <div id="post-div">
-                        <label htmlFor="post-markdown">Post</label>
-                        <textarea name="post" id="post-markdown" cols="30" rows="40" onChange={parseMD}></textarea>
-                    </div>
-                </section>
-                <section id="render"></section>
-            </body>
-        </>
+                <div id="post-div">
+                    <label htmlFor="post-markdown">Post</label>
+                    <textarea name="post" id="post-markdown" cols="30" rows="40" onChange={parseMD}></textarea>
+                </div>
+            </section>
+            <section id="render-section"><article id="render"></article></section>
+        </div>
     );
 }
 
